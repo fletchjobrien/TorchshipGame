873bb2bee193abcd0cba838cce426ef0ddbc3275
fletchjobrien <97412458+fletchjobrien@users.noreply.github.com>
2025-05-14 10:38:18 -0400

    Delete Space.py

--- Space.py
+++
@@ -1,8 +0,0 @@
-# space object
-
-class Space:
- def __init__(self, terrain, structure, coordinate, occupier):
-  self.terrain = terrain
-  self.coordinate = coordinate
-  self.structure = structure
-  self.occupier = occupier
