8ac0f9c82015ee1f6ec51a909227f05856e5b20d
fletchjobrien <97412458+fletchjobrien@users.noreply.github.com>
2025-05-14 10:38:25 -0400

    Delete Weapon.py

--- Weapon.py
+++
@@ -1,9 +0,0 @@
-# Weapon obj
-class Weapon:
- def __init__(self, name, aoe, range, dmg, knockback, accuracy):
-  self.name = name
-  self.aoe = aoe
-  self.range = range
-  self.dmg = dmg
-  self.knockback = knockback
-  self.accuracy = accuracy
